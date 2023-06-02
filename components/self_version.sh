92e935ee8c0d58337aa3d86d9bc4c334b377739d
Stephan Wendel <me@stephanwe.de>
2023-06-03 00:21:06 +0200

    chore:wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

---
+++ components/self_version.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#### Webcamd Core Application.
+
+#### crowsnest - A webcam Service for multiple Cams and Stream Services.
+####
+#### Written by Stephan Wendel aka KwadFan <me@stephanwe.de>
+#### Copyright 2021 - till today
+#### https://github.com/mainsail-crew/crowsnest
+####
+#### This File is distributed under GPLv3
+####
+
+# shellcheck enable=require-variable-braces
+
+# Exit upon Errors
+set -Ee
+
+self_version() {
+    pushd "${CN_WORKDIR_PATH}" &> /dev/null
+    git describe --always --tags
+    popd &> /dev/null
+}
