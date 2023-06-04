cbd3cc95db82b10644b31738a3562fdbb28c73f5
Stephan Wendel <me@stephanwe.de>
2023-06-04 20:42:27 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/config_parser.sh
+++ components/config_parser.sh
@@ -56,7 +56,7 @@ cn_set_self_config() {
     done
 
     for expose_var in "${config[@]}"; do
-        echo "${expose_var}" | tr -d "'"
+        echo "${expose_var}" | tr -d "'" | xargs -I {} declare -x -r {}
     done
 }
 
