5d1ba3af5cf3239851448686d2264991f493285d
Stephan Wendel <me@stephanwe.de>
2023-06-04 20:00:54 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/config_parser.sh
+++ components/config_parser.sh
@@ -48,11 +48,11 @@ cn_set_self_config() {
     local var_name
     for var in $(cn_get_self_config); do
         var_name="${var}"
-        declare "${var_name}"
+        declare "${var_name}"=""
         var="${var/CN_SELF_/}"
         var="${var,,}"
         echo "${var}"
-        # export "${var_name}"="$(cn_get_param "crowsnest" "${var}")"
+        eval "${var_name}"="$(cn_get_param "crowsnest" "${var}")"
         echo "${var_name}"
     done
 }
