c4f511caa8ab6276a9e43cd4b3f4217648b3ff8f
Stephan Wendel <me@stephanwe.de>
2023-06-04 12:44:11 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/self_version.sh
+++ components/self_version.sh
@@ -24,5 +24,6 @@ cn_self_version() {
 
 cn_self_version_msg() {
     printf "%s\n" "${CN_SELF_TITLE}"
-    printf "\tVersion: %s\n" "$(cn_self_version)"
+    printf "\n\tVersion: %s\n" "$(cn_self_version)"
+    printf "\n"
 }
