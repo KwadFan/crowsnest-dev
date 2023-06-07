fdb59b4b4daa1e61c39ec6d2b16addcd5c869f42
Stephan Wendel <me@stephanwe.de>
2023-06-07 20:52:04 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/config_parser.sh
+++ components/config_parser.sh
@@ -19,6 +19,7 @@ set -Ee
 cn_check_config_exist() {
     if [[ ! -f "${CROWSNEST_CONFIG_FILE}" ]]; then
         cn_log_msg "ERROR: Given configuration file '${CROWSNEST_CONFIG_FILE}' doesn't exist!"
+        cn_log_msg "Stoping $(basename "${0}")"
         exit 1
     fi
 }
