2c820c0d57ff1f38908d118562789f7731eefa80
Stephan Wendel <me@stephanwe.de>
2023-06-07 22:17:20 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/config_parser.sh
+++ components/config_parser.sh
@@ -30,10 +30,10 @@ cn_get_all_sections() {
 
 cn_set_cam_sections() {
     local -a name_spaces
-    name_spaces=("$(
+    mapfile -t name_spaces < <(
         crudini --existing=file --get "${CROWSNEST_CONFIG_FILE}" | \
         sed '/crowsnest/d;s/cam//' \
-    )")
+        )
     declare -g -r "CN_CONFIGURED_CAMS=${name_spaces[*]}"
 }
 
