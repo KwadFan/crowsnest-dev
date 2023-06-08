644778c24263192d302ce9de509442afc1e89b58
Stephan Wendel <me@stephanwe.de>
2023-06-08 13:13:35 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/arg_parser.sh
+++ components/arg_parser.sh
@@ -16,37 +16,6 @@
 # Exit upon Errors
 set -Ee
 
-
-cn_missing_args_msg() {
-    printf "%s\n" "${CN_SELF_TITLE}"
-    printf "\nERROR: Missing arguments!\n"
-    printf "\n\tTry: crowsnest -h\n"
-    printf "\n"
-}
-
-cn_wrong_args_msg() {
-    printf "%s\n" "${CN_SELF_TITLE}"
-    printf "\nERROR: Wrong Arguments!\n"
-    printf "\n\tTry: crowsnest -h\n"
-    printf "\n"
-}
-
-cn_help_msg() {
-    printf "%s\n" "${CN_SELF_TITLE}"
-    printf "\nUsage:\t crowsnest [Options]"
-    printf "\n\t\t-h Prints this help."
-    printf "\n\t\t-v Prints Version of crowsnest."
-    printf "\n\t\t-c </path/to/configfile>\n\t\t\tPath to your webcam.conf\n"
-    printf "\n"
-}
-
-cn_missing_cfg_path() {
-    printf "%s\n" "${CN_SELF_TITLE}"
-    printf "\nERROR: No configuration file path specified!\n"
-    printf "\n\tTry: crowsnest -c /path/to/configfile\n"
-    printf "\n"
-}
-
 cn_init_argparse() {
     if [ "$#" -eq 0 ]; then
         cn_missing_args_msg
