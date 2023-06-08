778673042318a5dc741775b3f994e1317a7ccdee
Stephan Wendel <me@stephanwe.de>
2023-06-08 11:19:43 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/config_parser.sh
+++ components/config_parser.sh
@@ -95,7 +95,7 @@ cn_set_cam_config() {
 init_config_parse() {
     cn_check_config_exist
 
-    bg cn_set_config "crowsnest" "CN_SELF_"
+    (cn_set_config "crowsnest" "CN_SELF_" &)
 
     cn_set_cam_sections
 
