fd440135181babd2db61d941f17874ba9e7f9c63
Stephan Wendel <me@stephanwe.de>
2023-06-08 13:45:27 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/log_config_file.sh
+++ components/log_config_file.sh
@@ -17,16 +17,36 @@
 set -Ee
 
 
+# cn_print_cfg() {
+#     local prefix
+#     prefix="$(date +'[%D %T]')"
+#     cn_log_msg "-------- Configfile: '${CN_CONFIG_FILE}' --------"
+#     (sed '/^#.*/d;/./,$!d' | cut -d'#' -f1) < "${CN_CONFIG_FILE}" | \
+#     while read -r line; do
+#         if [[ -n "${CN_SELF_LOG_PATH}" ]]; then
+#             printf "%s\t%s\n" "${prefix}" "${line}" >> "${CN_SELF_LOG_PATH}"
+#         fi
+#         printf "\t%s\n" "${line}"
+#     done
+# }
+
 cn_print_cfg() {
-    local prefix
+    local cfg prefix
     prefix="$(date +'[%D %T]')"
     cn_log_msg "-------- Configfile: '${CN_CONFIG_FILE}' --------"
-    (sed '/^#.*/d;/./,$!d' | cut -d'#' -f1) < "${CN_CONFIG_FILE}" | \
-    while read -r line; do
+#     (sed '/^#.*/d;/./,$!d' | cut -d'#' -f1) < "${CN_CONFIG_FILE}" | \
+#     while read -r line; do
+#         if [[ -n "${CN_SELF_LOG_PATH}" ]]; then
+#             printf "%s\t%s\n" "${prefix}" "${line}" >> "${CN_SELF_LOG_PATH}"
+#         fi
+#         printf "\t%s\n" "${line}"
+#     done
+    mapfile cfg < "${CN_CONFIG_FILE}"
+    for i in "${cfg[@]}"; do
         if [[ -n "${CN_SELF_LOG_PATH}" ]]; then
-            printf "%s\t%s\n" "${prefix}" "${line}" >> "${CN_SELF_LOG_PATH}"
+            printf "%s\t%s\n" "${prefix}" "${i}" >> "${CN_SELF_LOG_PATH}"
         fi
-        printf "\t%s\n" "${line}"
+        printf "\t%s\n" "${i}"
     done
 }
 
