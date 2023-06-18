c151f69f912c6764bfd68648ab8d24334c8e550b
Stephan Wendel <me@stephanwe.de>
2023-06-18 10:59:47 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/logging.sh
+++ components/logging.sh
@@ -67,7 +67,7 @@ cn_log_output() {
 cn_log_err_dump() {
     prefix="DUMP:"
     while read -r line; do
-        line="${line@@[-|:]@\t}"
+        line="${line//[-|:]/\\t}"
 
         if [[ "${CN_SELF_LOG_LEVEL}" = "debug" ]]; then
             cn_log_msg "${prefix} ${line}"
