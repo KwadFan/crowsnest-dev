34bddf5914a8d1686591540806323e30427f7e9d
Stephan Wendel <me@stephanwe.de>
2023-06-18 21:51:05 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/log_avail_cams.sh
+++ components/log_avail_cams.sh
@@ -16,24 +16,6 @@
 # Exit upon Errors
 set -Ee
 
-cn_print_cams() {
-    local total v4l
-    v4l="$(find /dev/v4l/by-id/ -iname "*index0" 2> /dev/null | wc -l)"
-    total="$((v4l+($(detect_libcamera))))"
-    if [ "${total}" -eq 0 ]; then
-        cn_log_msg "ERROR: No usable Devices Found. Stopping $(basename "${0}")."
-        exit 1
-    else
-        cn_log_msg "INFO: Found ${total} total available Device(s)"
-    fi
-    if [[ "$(detect_libcamera)" -ne 0 ]]; then
-        cn_log_msg "Detected 'libcamera' device -> $(get_libcamera_path)"
-    fi
-    if [[ -d "/dev/v4l/by-id/" ]]; then
-        detect_avail_cams
-    fi
-}
-
 cn_get_dev_count() {
     local total
     total="$(($(cn_get_libcamera_dev_count)+$(cn_get_uvc_dev_count)))"
@@ -45,6 +27,7 @@ cn_print_dev_count() {
         cn_camera_count_msg "$(cn_get_dev_count)"
     else
         cn_no_usable_cams_found_msg
+        exit 1
     fi
 }
 
