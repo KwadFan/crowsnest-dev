298ab8039e599162efae8b145a6256723c4eedb1
Stephan Wendel <me@stephanwe.de>
2023-06-18 19:02:17 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/hw_libcamera.sh
+++ components/hw_libcamera.sh
@@ -16,7 +16,25 @@
 # Exit upon Errors
 set -Ee
 
+CN_LIBCAMERA_BIN_PATH=""
+CN_LIBCAMERA_DEV_PATH=""
 
+
+cn_check_libcamera_hello_bin() {
+    local bin
+    bin="$(command -v libcamera-hello)"
+    if [[ -x "${bin}" ]]; then
+        CN_LIBCAMERA_BIN_PATH="${bin}"
+    else
+        CN_LIBCAMERA_BIN_PATH="null"
+    fi
+    declare -gr CN_LIBCAMERA_BIN_PATH
+}
+
+
+cn_init_hw_libcamera() {
+    cn_check_libcamera_hello_bin
+}
 
 if [[ "${CN_DEV_MSG}" = "1" ]]; then
     printf "Sourced component: hw_libcamera.sh\n"
