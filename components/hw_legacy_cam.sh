0fc26a5ff6a1cf9754d70c81c21b97371e39eb03
Stephan Wendel <me@stephanwe.de>
2023-06-19 21:49:05 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/hw_legacy_cam.sh
+++ components/hw_legacy_cam.sh
@@ -16,7 +16,16 @@
 # Exit upon Errors
 set -Ee
 
+cn_init_hw_legacy () {
 
+
+
+    if [[ "${CN_DEV_MSG}" = "1" ]]; then
+        printf "hw_uvc_dev:\n###########\n"
+        declare -p | grep "CN_LEGACY"
+        printf "###########\n"
+    fi
+}
 
 if [[ "${CN_DEV_MSG}" = "1" ]]; then
     printf "Sourced component: hw_legacy_cam.sh\n"
