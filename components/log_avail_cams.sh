bbfec4d08d63eb7f26015a48c31a0db4fa6d99c4
Stephan Wendel <me@stephanwe.de>
2023-07-22 13:06:48 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/log_avail_cams.sh
+++ components/log_avail_cams.sh
@@ -75,10 +75,10 @@ cn_log_uvc_dev() {
 
 
         cn_supported_formats_msg
-        cn_get_supported_formats "${device}" | cn_log_v4l2ctl_output
+        #cn_get_supported_formats "${device}" | cn_log_v4l2ctl_output
 
         cn_supported_ctrls_msg
-        cn_get_supported_ctrls "${device}" | cn_log_v4l2ctl_output
+        #cn_get_supported_ctrls "${device}" | cn_log_v4l2ctl_output
     done
 }
 
