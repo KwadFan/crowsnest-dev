b822fb71a77a43d1e7e23362900189c6a5d8fef4
Stephan Wendel <me@stephanwe.de>
2023-08-09 19:40:52 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/init_stream.sh
+++ components/init_stream.sh
@@ -24,6 +24,12 @@ cn_get_streamer() {
 }
 
 cn_init_streams() {
+    if [[ "${CN_DEV_MSG}" = "1" ]]; then
+        printf "init_stream:\n###########\n"
+        declare -p | grep "CN_AVAIL_BACKENDS"
+        printf "###########\n"
+    fi
+
 
     cn_init_streams_msg_header
 
@@ -41,3 +47,41 @@ cn_init_streams() {
 if [[ "${CN_DEV_MSG}" = "1" ]]; then
     printf "Sourced component: init_stream.sh\n"
 fi
+
+
+# function construct_streamer {
+#     local cams sleep_pid
+#     # See configparser.sh L#53
+#     log_msg "Try to start configured Cams / Services..."
+#     for cams in $(configured_cams); do
+#         mode="$(get_param "cam ${cams}" mode)"
+#         check_section "${cams}"
+#         case ${mode} in
+#             [mM]ulti | camera-streamer)
+#                 if [[ "$(is_raspberry_pi)" = "1" ]] && [[ "$(is_ubuntu_arm)" = "0" ]]; then
+#                     MULTI_INSTANCES+=( "${cams}" )
+#                 else
+#                     log_msg "WARN: Mode 'multi' is not supported on your device!"
+#                     log_msg "WARN: Falling back to Mode 'mjpg'"
+#                     MJPG_INSTANCES+=( "${cams}" )
+#                 fi
+#             ;;
+#             mjpg | mjpeg | ustreamer)
+#                 MJPG_INSTANCES+=( "${cams}" )
+#             ;;
+#             ?|*)
+#                 unknown_mode_msg
+#                 MJPG_INSTANCES+=( "${cams}" )
+
+#             ;;
+#         esac
+#     done
+#     if [ "${#MULTI_INSTANCES[@]}" != "0" ]; then
+#         run_multi "${MULTI_INSTANCES[*]}"
+#     fi
+#     if [ "${#MJPG_INSTANCES[@]}" != "0" ]; then
+#         run_mjpg "${MJPG_INSTANCES[*]}"
+#     fi
+#     sleep 2 & sleep_pid="$!" ; wait "${sleep_pid}"
+#     log_msg " ... Done!"
+# }
