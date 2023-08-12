d2fff932ef43eab239bf144216c749219990bf2b
Stephan Wendel <me@stephanwe.de>
2023-08-12 09:34:51 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

---
+++ components/ustreamer.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#### ustreamer.sh - exec ustreamer
+
+#### crowsnest - A webcam Service for multiple Cams and Stream Services.
+####
+#### Written by Stephan Wendel aka KwadFan <me@stephanwe.de>
+#### Copyright 2021 - till today
+#### https://github.com/mainsail-crew/crowsnest
+####
+#### This File is distributed under GPLv3
+####
+
+# shellcheck enable=require-variable-braces
+
+# Exit upon Errors
+set -Ee
+
+
+
+if [[ "${CN_DEV_MSG}" = "1" ]]; then
+    printf "Sourced component: ustreamer.sh\n"
+fi
