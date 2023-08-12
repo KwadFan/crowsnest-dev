25ef06d3d4643fc39e7961bf52cb7327259d666b
Stephan Wendel <me@stephanwe.de>
2023-08-12 12:28:57 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/logging.sh
+++ components/logging.sh
@@ -75,7 +75,7 @@ cn_log_output() {
     prefix="DEBUG: ${1}"
     while read -r line; do
         ## Ustreamer workaround
-        line="${line/^\=*/}"
+        line="${line/===*/}"
         if [[ -n "${line}" ]] && [[ "${CN_SELF_LOG_LEVEL}" = "debug" ]]; then
             cn_log_msg "${prefix}: ${line}"
         fi
