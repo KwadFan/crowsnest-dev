e09d558a055052c845074810c9dce10bf1078196
Stephan Wendel <me@stephanwe.de>
2023-08-12 11:33:39 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/ustreamer.sh
+++ components/ustreamer.sh
@@ -44,7 +44,6 @@ cn_exec_ustreamer() {
 
 cn_ustreamer_loop() {
     (echo "${2}" | xargs "${CN_USTREAMER_BIN_PATH}" 2>&1 \
-    | sed "/^=/d" \
     | cn_log_output "ustreamer [cam ${1}]") \
     || cn_ustreamer_failed_msg "${1}"
 }
