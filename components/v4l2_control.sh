8649a0cf5c9948df40aa776717e939cb53738da6
Stephan Wendel <me@stephanwe.de>
2023-08-19 12:08:46 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/v4l2_control.sh
+++ components/v4l2_control.sh
@@ -80,8 +80,8 @@ cn_init_v4l2ctl() {
     if [[ "${CN_DEV_MSG}" = "1" ]]; then
         printf "v4l2_control:\n###########\n"
         printf "Configured cams: %s\n" "${CN_CONFIGURED_CAMS[@]}"
+        declare -p | grep "CN_V4L2CTL_BIN_PATH"
         declare -p | grep "CN_CAM_.*_V4L2CTL"
-        # declare -p | grep "CN_CAM_.*_V4L2CTL_ARRAY"
         printf "###########\n"
     fi
 }
