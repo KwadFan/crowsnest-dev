1a42dbaff67fa73fd8f9caa0133456b911e594c8
Stephan Wendel <me@stephanwe.de>
2023-08-26 11:03:29 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/messages.sh
+++ components/messages.sh
@@ -241,8 +241,6 @@ cn_v4l2ctl_set_giveup_msg() {
 cn_v4l2ctl_allowed_range_msg() {
     cn_log_info_msg "Value might be out of allowed range."
     cn_log_info_msg "Please see 'Supported Controls:' block of this cam for details!"
-    # put some whitespace here
-    cn_log_msg " "
 }
 
 cn_v4l2ctl_ctrl_not_supported_msg() {
