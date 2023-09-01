a89680fe8cd4896934f9299a7220904228749872
Stephan Wendel <me@stephanwe.de>
2023-09-01 23:33:30 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/init_stream.sh
+++ components/init_stream.sh
@@ -65,6 +65,9 @@ cn_init_streams() {
     done
     if [[ "${#CONFIGURED_CAMS[@]}" = "${instance_count}" ]]; then
         cn_log_info_msg "Configured streams initiated ..."
+        # put some whitespace here
+        cn_log_msg " "
+        # give some head room for watchdog
         sleep 0.5
     fi
 
