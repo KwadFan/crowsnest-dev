f77d628be705858a341441086ac22477ebb5db82
Stephan Wendel <me@stephanwe.de>
2023-09-02 14:51:58 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/watchdog.sh
+++ components/watchdog.sh
@@ -67,7 +67,7 @@ cn_watchdog_runtime() {
     for x in "${CN_WATCHDOG_DEVICE_ARRAY[@]}"; do
         if [[ "${CN_DEV_MSG}" = "1" ]]; then
             printf "watchdog runtime:\n###########\n"
-            printf "Checking device: %s" "${x}"
+            printf "Checking device: %s\n" "${x}"
             printf "###########\n"
         fi
         real_path="$(cn_watchdog_get_real_path "${x}")"
