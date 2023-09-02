92466d439e67411e7be25e7f7de716e606083c7b
Stephan Wendel <me@stephanwe.de>
2023-09-02 15:10:37 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/watchdog.sh
+++ components/watchdog.sh
@@ -55,22 +55,12 @@ cn_watchdog_debug_print_devices() {
     fi
 }
 
-cn_watchdog_get_real_path() {
-    local path
-    path="$(readlink "${1}" | sed 's/^\.\.\/\.\./\/dev/')"
-    printf "%s" "${path}"
-}
-
 cn_watchdog_runtime() {
     sleep "${CN_WATCHDOG_SLEEP_TIME}"
     for x in "${CN_WATCHDOG_DEVICE_ARRAY[@]}"; do
-        # if [[ "${CN_DEV_MSG}" = "1" ]]; then
-        #     printf "watchdog runtime:\n###########\n"
-        #     printf "Checking device: %s\n" "${x}"
-        #     printf "###########\n"
-        # fi
+        # filter to by_id only!
         if [[ "${x}" =~ "/dev/v4l/by-id" ]] && [[ ! -e "${x}" ]]; then
-            cn_log_warn_msg "Lost device(s) '${x}' ( $(cn_watchdog_get_real_path "${x}") ) !!!!"
+            cn_log_warn_msg "Lost device(s) '${x}' !!!!"
         fi
     done
 }
