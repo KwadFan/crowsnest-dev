7955929e9cfe399d4f63b3021ebab124ab9146bf
Stephan Wendel <me@stephanwe.de>
2023-09-03 11:21:02 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/watchdog.sh
+++ components/watchdog.sh
@@ -64,7 +64,8 @@ cn_watchdog_runtime() {
             cn_log_msg "${prefix} Lost device '${x}' !!!!"
             cn_log_info_msg "Next check in ${CN_WATCHDOG_SLEEP_TIME} seconds ..."
             cn_log_msg " "
-        else
+        elif [[ "${x}" =~ "/dev/v4l/by-id" ]] && [[ -e "${x}" ]] && \
+            [[ "${#lost_dev[*]}" -gt "0" ]]; then
             lost_dev=( "${lost_dev[@]/${x}}" )
             cn_log_msg " "
             cn_log_msg "${prefix} Device '${x}' returned ..."
