be7877a0a709fdd611d558001ed8afe367eb1591
Stephan Wendel <me@stephanwe.de>
2023-09-16 08:21:19 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/deep_config_check.sh
+++ components/deep_config_check.sh
@@ -20,19 +20,12 @@ cn_deep_config_check_mode() {
     local mode_sect
     mode_sect="CN_CAM_${1}_MODE"
     if [[ "${CN_AVAIL_BACKENDS[*]}" =~ ${!mode_sect} ]]; then
-
         cn_log_check_state_msg "mode" "${!mode_sect}" "0"
-
         cn_log_msg " "
-
     else
-
         cn_log_check_state_msg "mode" "${!mode_sect}" "1"
-
         cn_log_check_mode_failed_msg "${!mode_sect}"
-
         cn_deep_config_check_failed
-
     fi
 }
 
@@ -42,16 +35,11 @@ cn_deep_config_check_port() {
     if [[ "${!port}" =~ [0-9] ]] \
     && [[ "${!port}" -gt "0" ]] \
     && [[ "${!port}" -le "65535" ]]; then
-
         cn_log_check_state_msg "port" "${!port}" "0"
-
         cn_log_msg " "
-
     else
         cn_log_check_state_msg "port" "${!port}" "1"
-
         cn_log_check_port_failed_msg "${!port}"
-
         cn_deep_config_check_failed
     fi
 }
@@ -66,35 +54,22 @@ cn_deep_config_check_device() {
 
     elif [[ "${!device}" =~ /dev/video[0-9] ]] \
     && [[ "${CN_UVC_VALID_DEVICES[*]}" =~ ${!device} ]]; then
-
         cn_log_check_dev_path_msg "${!device}"
-
     elif [[ "${CN_UVC_VALID_DEVICES[*]}" =~ ${!device} ]]; then
-
         cn_log_check_state_msg "device" "${!device}" "0"
-
         cn_log_msg " "
-
     elif [[ "${!device}" =~ "/base/soc" ]]; then
         if [[ "${!device}" = "${CN_LIBCAMERA_DEV_PATH}" ]]; then
-
             cn_log_check_state_msg "device" "${!device}" "0"
-
             cn_log_msg " "
-
         else
-
             cn_log_check_state_msg "device" "${!device}" "1"
-
             cn_check_raspicam_faq_msg "${!device}"
-
             cn_deep_config_check_failed
-
         fi
 
     else
         cn_log_check_state_msg "device" "${!device}" "1"
-
         cn_deep_config_check_failed
     fi
 }
@@ -103,19 +78,12 @@ cn_deep_config_check_resolution() {
     local resolution
     resolution="CN_CAM_${1}_RESOLUTION"
     if [[ "${!resolution}" =~ ^[0-9]*[0-9]+x+[0-9]*[0-9]$ ]]; then
-
         cn_log_check_state_msg "resolution" "${!resolution}" "0"
-
         cn_log_msg " "
-
     else
-
         cn_log_check_state_msg "resolution" "${!resolution}" "1"
-
         cn_log_check_resolution_msg
-
         cn_deep_config_check_failed
-
     fi
 }
 
@@ -123,19 +91,12 @@ cn_deep_config_check_max_fps() {
     local max_fps
     max_fps="CN_CAM_${1}_MAX_FPS"
     if [[ "${!max_fps}" =~ ^[0-9]+$ ]]; then
-
         cn_log_check_state_msg "max_fps" "${!max_fps}" "0"
-
         cn_log_msg " "
-
     else
-
         cn_log_check_state_msg "max_fps" "${!max_fps}" "1"
-
         cn_log_check_max_fps_msg
-
         cn_deep_config_check_failed
-
     fi
 }
 
@@ -145,7 +106,6 @@ cn_deep_config_check_rtsp() {
 
 cn_deep_config_check_failed() {
     cn_stopped_msg
-
     exit 1
 }
 
@@ -179,4 +139,8 @@ cn_init_deep_config_check() {
 
 if [[ "${CN_DEV_MSG}" = "1" ]]; then
     printf "Sourced component: deep_config_check.sh\n"
+fi
+
+if [[ "${BASH_SOURCE[0]}" = "${0}" ]]; then
+    cn_log_msg "DO NO EXECUTE ${0} ON ITS OWN!"
 fi
