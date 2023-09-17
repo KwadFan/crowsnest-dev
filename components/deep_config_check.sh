935acf2f274fe4f61c339ee887e7adcede929ded
Stephan Wendel <me@stephanwe.de>
2023-09-17 14:34:06 +0200

    chore: wip
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- components/deep_config_check.sh
+++ components/deep_config_check.sh
@@ -21,7 +21,6 @@ cn_deep_config_check_mode() {
     mode_sect="CN_CAM_${1}_MODE"
     if [[ "${CN_AVAIL_BACKENDS[*]}" =~ ${!mode_sect} ]]; then
         cn_log_check_state_msg "mode" "${!mode_sect}" "0"
-        cn_log_msg " "
     else
         cn_log_check_state_msg "mode" "${!mode_sect}" "1"
         cn_log_check_mode_failed_msg "${!mode_sect}"
@@ -36,7 +35,6 @@ cn_deep_config_check_port() {
     && [[ "${!port}" -gt "0" ]] \
     && [[ "${!port}" -le "65535" ]]; then
         cn_log_check_state_msg "port" "${!port}" "0"
-        cn_log_msg " "
     else
         cn_log_check_state_msg "port" "${!port}" "1"
         cn_log_check_port_failed_msg "${!port}"
@@ -56,11 +54,9 @@ cn_deep_config_check_device() {
         cn_log_check_dev_path_msg "${!device}"
     elif [[ "${CN_UVC_VALID_DEVICES[*]}" =~ ${!device} ]]; then
         cn_log_check_state_msg "device" "${!device}" "0"
-        cn_log_msg " "
     elif [[ "${!device}" =~ "/base/soc" ]]; then
         if [[ "${!device}" = "${CN_LIBCAMERA_DEV_PATH}" ]]; then
             cn_log_check_state_msg "device" "${!device}" "0"
-            cn_log_msg " "
         else
             cn_log_check_state_msg "device" "${!device}" "1"
             cn_check_raspicam_faq_msg "${!device}"
@@ -77,7 +73,6 @@ cn_deep_config_check_resolution() {
     resolution="CN_CAM_${1}_RESOLUTION"
     if [[ "${!resolution}" =~ ^[0-9]*[0-9]+x+[0-9]*[0-9]$ ]]; then
         cn_log_check_state_msg "resolution" "${!resolution}" "0"
-        cn_log_msg " "
     else
         cn_log_check_state_msg "resolution" "${!resolution}" "1"
         cn_log_check_resolution_msg
@@ -90,7 +85,6 @@ cn_deep_config_check_max_fps() {
     max_fps="CN_CAM_${1}_MAX_FPS"
     if [[ "${!max_fps}" =~ ^[0-9]+$ ]]; then
         cn_log_check_state_msg "max_fps" "${!max_fps}" "0"
-        cn_log_msg " "
     else
         cn_log_check_state_msg "max_fps" "${!max_fps}" "1"
         cn_log_check_max_fps_msg
